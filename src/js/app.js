876bcd3d5a4db111bd1ca5df89ede454ae598d91
rsgm1978 <159719756+rsgm1978@users.noreply.github.com>
2024-08-07 19:24:48 +0000

    conditionalprof

--- src/js/app.js
+++ src/js/app.js
@@ -33,14 +33,26 @@ function render(variables = {}) {
   document.querySelector("#widget_content").innerHTML = `<div class="widget">
             ${cover}
           <img src="${variables.avatarURL}" class="photo" />
-          <h1>Lucy Boilett</h1>
-          <h2>Web Developer</h2>
-          <h3>Miami, USA</h3>
-          <ul class="position-right">
-            <li><a href="https://twitter.com/4geeksacademy"><i class="fab fa-twitter"></i></a></li>
-            <li><a href="https://github.com/4geeksacademy"><i class="fab fa-github"></i></a></li>
-            <li><a href="https://linkedin.com/school/4geeksacademy"><i class="fab fa-linkedin"></i></a></li>
-            <li><a href="https://instagram.com/4geeksacademy"><i class="fab fa-instagram"></i></a></li>
+          <h1>${variables.name ? variables.name : "Name"} ${
+    variables.lastname ? variables.lastname : "Lastname"
+  }</h1>
+          <h2>${variables.role ? variables.role : " "}</h2>
+          <h3>${variables.city ? variables.city : " "}, ${
+    variables.country ? variables.country : " "
+  }</h3>
+          <ul class="position-rigth">
+            <li><a href=${
+              variables.twitter ? variables.twitter : " "
+            }><i class="fab fa-twitter"></i></a></li>
+            <li><a href=${
+              variables.github ? variables.github : " "
+            }><i class="fab fa-github"></i></a></li>
+            <li><a href=${
+              variables.linkedink ? variables.linkedink : " "
+            }><i class="fab fa-linkedin"></i></a></li>
+            <li><a href=${
+              variables.instagram ? variables.instagram : " "
+            }><i class="fab fa-instagram"></i></a></li>
           </ul>
         </div>
     `;
